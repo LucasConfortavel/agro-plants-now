e1208b951704ba6a5a3baf038ea6e3c65f4a2822
YunoKamai <sabrinaluana3@gmail.com>
2025-02-17 17:12:37 -0400

    SAB_TASK12

--- VIEW/vend/Cupom_vendedor.php
+++ VIEW/vend/Cupom_vendedor.php
@@ -1,6 +1,96 @@
 <?php
 
-require "Menu_vend.html";
+include "../../INCLUDE/Menu_vend.php";
+
+
+
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agro Plants NOW</title>
+    <link rel="stylesheet" href="../global-index/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <main class="jp_main-content">
+        </div>
+        <div class="sab-engloba-tudo">
+            <div class="sab-fundo">
+                <div class="sab-icones">
+                    <div class="sab-icone">
+                        <i class="fa-regular fa-bell"></i>
+                    </div>
+                    <div class="sab-icone2">
+                        <i class="fa-regular fa-user"></i>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="sab-input">
+                <input type="search" id="sab-site-search" placeholder="Pesquise por um cupom">
+                    
+                </input>
+                <i class="fa-solid fa-magnifying-glass sab-icone-pesquisa"></i>
+        
+            </div>
+            <div class="sab-botão">
+                <button>Cadastrar cupom</button>
+            </div>
+            <div class="sab-fundo-tabela">
+                
+                <table class="sab-tabela">
+                    
+                    <thead>
+                        <tr>
+                            <th>
+                                Código
+                            </th>
+                            <th>
+                                Porcentagem
+                            </th>
+                            <th>
+                                Data
+                            </th>
+                            <th>
+                                Estado
+                            </th>
+                            <th>
+                                
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td>#paulorojas100</td>
+                        <td class="sab-aqua">100%</td>
+                        <td>16/08</td>
+                        <td>Ativo</td>
+                        <td><i class="fa-solid fa-circle-minus"></i></td>
+                    </tbody>
+                </table>
+                <div class="sab-display">
+                    <div class="sab-botao-tabela">
+                        <i class="fa-solid fa-arrow-right"></i>
+
+                    </div>
+                </div>
+            
+            </div>
+        </div> 
+    
+    
+
+    </main>
+</body>
+</html>
+
+
+    
+    
