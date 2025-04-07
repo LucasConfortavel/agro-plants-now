79cd60fe976e6074c2eba8f059a48491dfd19a99
YunoKamai <sabrinaluana3@gmail.com>
2025-04-07 15:26:19 -0400

    task05

--- VIEW/vend/Cupom_vendedor.php
+++ VIEW/vend/Cupom_vendedor.php
@@ -9,7 +9,7 @@ include "../../INCLUDE/Menu_vend.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agro Plants NOW</title>
-    <link rel="stylesheet" href="/cup_vend/PUBLIC/css/sab_cupom.css">
+    <link rel="stylesheet" href="/projeto_agro_plants_now1/PUBLIC/css/sab_cupom.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
