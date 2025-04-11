af26c69deeb930a23026cd0fd028f907c0f26e34
YunoKamai <sabrinaluana3@gmail.com>
2025-04-11 15:52:51 -0400

    arquitetura-mvc_2

--- VIEW/adm/ajustes_segurity.php
+++ VIEW/adm/ajustes_segurity.php
@@ -27,8 +27,8 @@ include "../../INCLUDE/Menu_vend.php";
 
             <div class="jp_content">
                 <nav class="jp_tabs">
-                    <a href="informaçoes_adm.html" class="jp_tab">Informações</a>
-                    <a href="segurity.html" class="jp_tab jp_active">Segurança</a>
+                    <a href="ajustes_informaçoes_adm.php" class="jp_tab">Informações</a>
+                    <a href="ajustes_segurity.php" class="jp_tab jp_active">Segurança</a>
                 </nav>
 
                 <div class="jp_info-section">
