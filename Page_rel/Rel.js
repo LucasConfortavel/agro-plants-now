e7a6926cbf53a76e5a79cbe58d0edc497bd97bc7
Madarauos <paulootaviogalala@gmail.com>
2025-04-11 15:59:42 -0400

    commit page rel-vendas
    alterei a pagina de vendas  e estou atualmente arrumando de comissoes

--- Page_rel/Rel.js
+++ Page_rel/Rel.js
@@ -1,3 +1,80 @@
+const graficoMaiorValor = [
+    { month: 'Jan', valor: 35 },
+    { month: 'Fev', valor: 42 },
+    { month: 'Mar', valor: 25 },
+    { month: 'Abr', valor: 50 },
+    { month: 'Mai', valor: 55 },
+    { month: 'Jun', valor: 20 },
+    { month: 'Jul', valor: 15 },
+    { month: 'Ago', valor: 90 },
+    { month: 'Set', valor: 45 },
+    { month: 'Out', valor: 50 },
+    { month: 'Nov', valor: 20 },
+    { month: 'Dez', valor: 35 }
+];
+
+const miniGraficosValores = [
+    35, 42, 25, 50, 55, 20, 15, 90, 45, 50, 20, 35
+];
+
+function criarbarraGrafico() {
+    const chartContainer = document.getElementById('vendas-grafico');
+    const maxValor = Math.max(...graficoMaiorValor.map(item => item.valor));
+    
+    graficoMaiorValor.forEach(item => {
+        const barraTamanho = (item.valor / maxValor) * 235;
+        
+        const bar = document.createElement('div');
+        bar.className = 'bar';
+        bar.style.height = `${barraTamanho}px`;
+        
+        const label = document.createElement('div');
+        label.className = 'bar-label';
+        label.textContent = item.month;
+        
+        bar.appendChild(label);
+        chartContainer.appendChild(bar);
+    });
+}
+
+function cirarMiniGrafico() {
+    const miniChartContainer = document.getElementById('mini-grafico');
+    const maxValor = Math.max(...miniGraficosValores);
+    
+    miniGraficosValores.forEach(valor => {
+        const barraTamanho = (valor / maxValor) * 50;
+        
+        const bar = document.createElement('div');
+        bar.className = 'mini-bar';
+        bar.style.height = `${barraTamanho}px`;
+        
+        miniChartContainer.appendChild(bar);
+    });
+}
+
+window.onload = function() {
+    criarbarraGrafico();
+    cirarMiniGrafico();
+    
+
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function() {
+            tabs.forEach(t => t.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+    
+
+    const paginacaoItems = document.querySelectorAll('.paginacao span');
+    paginacaoItems.forEach(item => {
+        item.addEventListener('click', function() {
+            paginacaoItems.forEach(i => i.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+};
+
 document.addEventListener('DOMContentLoaded', function() {
     const hamburgerMenu = document.querySelector('.jp_hamburger-menu');
     const sidebar = document.querySelector('.jp_sidebar');
