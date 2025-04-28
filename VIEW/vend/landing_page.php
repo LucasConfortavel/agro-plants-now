3f636a98ed648c5fa7691d8243c1b8378043535e
Eduardo31-del <eduardoribeirolopes472@gmail.com>
2025-04-28 17:12:29 -0400

    adicionei ao mvc

---
+++ VIEW/vend/landing_page.php
@@ -0,0 +1,60 @@
+<?php
+
+include "../../INCLUDE/Menu_vend.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../PUBLIC/css/landing_pg.css">
+</head>
+<body >
+    <main class="jp-main-content">
+        <section class="er_cabecario">
+            <header class="er_menu">
+                    <nav class="er_nav-bar">
+                        <img class="er_logo" src="../PUBLIC/img/horizontal-logo 1.png" alt="">
+                        <ul class="er_nav-list">
+                            <li class="er_nav-item">
+                                <a href="" class="er_nav-link">Home</a>
+                            </li>
+                            <li class="er_nav-item">
+                                <a href="" class="er_nav-link">Sobre-nós</a>
+                            </li>
+                            <li class="er_nav-item">
+                                <a href="" class="er_nav-link">Contate-nos</a>
+                            </li>
+                        </ul>
+                        <button class="er_btn-padrao">
+                            Login
+                        </button>
+                    </nav>
+                </header>
+        
+            
+            
+                <div class="er_imagem">
+                    <img src="../PUBLIC/img/large-green-rice-field-with-green-rice-plants-rows 2.png" alt="" class="img-lp">
+                    <div class="img-container">
+
+                        <h1 class="er_texto">
+                            Seja um vendedor na maior distribuidora de insumos e serviços do agronegócio
+                        </h1>
+                
+                        <div class="er_btn-sonic">
+                            <button class="er_btn-sobre-nos">
+                                Sobre-nós
+                            </button>  
+                        </div>
+                    </div>
+                </div>
+        </section>
+    </main>  
+</body>
+</html>
