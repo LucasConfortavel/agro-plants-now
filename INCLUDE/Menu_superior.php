c91998fd7cfe753112378eeb9a61e52a21ff534e
Gustavo65157 <gustavosantosggu@gmail.com>
2025-05-09 15:05:11 -0400

    arrumei os titulos dos menus

--- INCLUDE/Menu_superior.php
+++ INCLUDE/Menu_superior.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agro Plants Now</title>
     <link rel="stylesheet" href="../PUBLIC/css/style_menu_superior.css">
 </head>
 <body>
