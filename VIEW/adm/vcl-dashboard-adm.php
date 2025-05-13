e59fc4c7fbbbdcb47f366310692ebda2ed60d992
luacarlos <santoslua226@gmail.com>
2025-05-13 14:34:30 -0400

    detalhes

--- VIEW/adm/vcl-dashboard-adm.php
+++ VIEW/adm/vcl-dashboard-adm.php
@@ -10,6 +10,7 @@
     <title>Dashboard de Vendas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../../PUBLIC/css/vcl-dashboard-style.css">
+    <link rel="stylesheet" href="../../PUBLIC/css/style_menu.css">
 </head>
 <body>
     
