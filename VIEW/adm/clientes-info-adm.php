d3cb65f8f45a6bb84fbb94ac0f64f4a444afb6f6
Eduardo31-del <eduardoribeirolopes472@gmail.com>
2025-05-15 17:08:39 -0400

    up-clientes-info

---
+++ VIEW/adm/clientes-info-adm.php
@@ -0,0 +1,24 @@
+<?php
+
+include "../../INCLUDE/Menu_adm.php";
+include "../btn/barra-pesquisa.php";
+include "../btn/btn-notificaçao.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <div class="er-box">
+        <a href="#"><img src="../../PUBLIC/img/Frame (3).svg" alt=""></a>
+
+        <img src="../../PUBLIC/img/user.svg" alt="">
+        <p></p>
+     </div>
+</body>
+</html>
