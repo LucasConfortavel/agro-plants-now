eb9b5b645dabc2e5c6cd2578acd034fc839f6646
YunoKamai <sabrinaluana3@gmail.com>
2025-05-28 16:56:45 -0400

    task01

---
+++ VIEW/pop-up/pop-up_remover_vendedor.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../PUBLIC/css/pop-up_remover_vendedor.css">
+    <title>Adicionar Serviço</title>
+</head>
+<body>
+    <div class="sab-container">
+        <div class="sab-tab-header">
+            <button class="sab-tab-button sab-active">Remover vendedor</button>
+        </div>
+        
+        <div class="sab-form-section">
+            <form>
+                <div class="sab-form-row">
+                    <div class="sab-form-group">
+                        <div class="sab-form-label-group">
+                            <span class="sab-required">*</span>
+                            <label class="sab-label-text">Obrigatório</label>
+                        </div>
+                        <input type="text" class="sab-form-control" placeholder="CPF">
+                    </div>
+                </div>
+            </form>
+            
+            <div class="sab-button-container">
+                <button type="button" class="sab-add-button">Avançar</button>
+            </div>
+        </div>
+        
+</body>
+</html>
