e9d07498883e61993858b9e629d44bff8558d5bb
eze-alrc <rocha.ezequiel.alves@gmail.com>
2025-06-11 16:56:09 -0400

    padronização img
    padronização das img nas telas php

--- VIEW/adm/produtos_administrador.php
+++ VIEW/adm/produtos_administrador.php
@@ -14,10 +14,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-
+img
     <main class="jp_main-content">
         
-        <?php $nome_produto = 'Nome do produto'; $imagem_produto = '../../PUBLIC/img/produto.png';?>
+        <?php $nome_produto = 'Nome do produto'; $imagem_produto = '../../PUBLIC/img/img_produto.png';?>
 
         <section class="ym_sectionProdutos">
 
