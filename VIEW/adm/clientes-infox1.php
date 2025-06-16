590e7c48a985fe7deac152093e7990f49d30b24e
Eduardo31-del <eduardoribeirolopes472@gmail.com>
2025-06-16 16:49:10 -0400

    up-x1

---
+++ VIEW/adm/clientes-infox1.php
@@ -0,0 +1,60 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../../PUBLIC/css/clientes-info.css">
+  
+</head>
+<body>
+    <main class="jp_main-content">
+        
+        <input  class="er_pesquisa-x1" type="text" name="search" id="search" placeholder="pesquise por um cliente"> <a href="" class="er_search-img"><img src="../../PUBLIC/img/lupa.png" alt=""></a>
+        
+        
+        
+        
+        <div class="er-box-x1">
+            <div class="er_nao-questione-x1">
+                <a href="#"><img class="er_seta-x1"  src="../../PUBLIC/img/Frame (3).svg" alt=""></a>
+
+                <div class="er-box-img-x1"> <img class="er_user-x1" src="../../PUBLIC/img/SVGRepo_iconCarrier (2).svg" alt=""></div>
+                
+                <p class="er_user-name-x1">Rafael Germinari</p>
+                <p class="er_user-date-x1">Cadastrado em 12/08/2024</p>
+            </div> 
+            
+            <form class="er_form-x1" action="">
+                <div class="er_wrap-input-x1">
+                    <span class="er_email-x1">E-mail:</span>
+                    <input class="er_input-email-x1" type="email" name="email" id="">
+                    <hr class="hr-x1">
+                </div>
+                <div class="er_wrap-input-x2 ">
+                    <span class="er_fone-x1">Telefone:</span>
+                    <input class="er_input-fone-x1" type="text" name="fone" id="">
+                    <hr class="hr2-x1">
+                </div>
+                <div class="er_wrap-input-x3">
+                    <span class="er_cpf-x1">Cpf:</span>
+                    <input class="er_input-cpf-x1" type="text" name="cpf" id="">
+                    <hr class="hr3-x1" >
+                </div>
+                <div class="er_wrap-input-x4">
+                    <span class="er_date-x1">Data de Nascimento:</span>
+                    <input class="er_input-date-x1" type="date" name="data" id="">
+                    <hr class="hr4-x1">
+                </div>
+                
+
+            </form>
+
+            <div class="er_btn-fixado-x1">
+                <a href="">Fixado</a>
+            </div>
+            
+        </div>
+    </main>
+</body>
+</html>
