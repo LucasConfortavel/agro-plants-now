53d76b98bc7567457b7d2f49a1f8ef14327f08b0
eze-alrc <rocha.ezequiel.alves@gmail.com>
2025-06-16 15:57:48 -0400

    padronização link img adm
    padronizei os links das imagens nas telas do adm

--- VIEW/adm/produtos_administrador.php
+++ VIEW/adm/produtos_administrador.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-img
+
     <main class="jp_main-content">
         
         <?php $nome_produto = 'Nome do produto'; $imagem_produto = '../../PUBLIC/img/img_produto.png';?>
