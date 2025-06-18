4d468c8537dba8c0c3814c8359303ca99cb42c5c
Eduardo31-del <eduardoribeirolopes472@gmail.com>
2025-06-18 17:02:33 -0400

    arrumando clientes-info

--- VIEW/adm/clientes-infox1.php
+++ VIEW/adm/clientes-infox1.php
@@ -1,3 +1,9 @@
+<?php
+
+include "../../INCLUDE/Menu_adm.php";
+
+?>
+
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -10,47 +16,47 @@
 <body>
     <main class="jp_main-content">
         
-        <input  class="er_pesquisa-x1" type="text" name="search" id="search" placeholder="pesquise por um cliente"> <a href="" class="er_search-img"><img src="../../PUBLIC/img/lupa.png" alt=""></a>
+        <input  class="er_pesquisa" type="text" name="search" id="search" placeholder="pesquise por um cliente"> <a href="" class="er_search-img"><img src="../../PUBLIC/img/lupa.png" alt=""></a>
         
         
         
         
-        <div class="er-box-x1">
-            <div class="er_nao-questione-x1">
-                <a href="#"><img class="er_seta-x1"  src="../../PUBLIC/img/Frame (3).svg" alt=""></a>
+        <div class="er-box">
+            <div class="er_nao-questione">
+                <a href="#"><img class="er_seta"  src="../../PUBLIC/img/Frame (3).svg" alt=""></a>
 
-                <div class="er-box-img-x1"> <img class="er_user-x1" src="../../PUBLIC/img/SVGRepo_iconCarrier (2).svg" alt=""></div>
+                <div class="er-box-img"> <img class="er_user" src="../../PUBLIC/img/SVGRepo_iconCarrier (2).svg" alt=""></div>
                 
                 <p class="er_user-name-x1">Rafael Germinari</p>
-                <p class="er_user-date-x1">Cadastrado em 12/08/2024</p>
+                <p class="er_user-date">Cadastrado em 12/08/2024</p>
             </div> 
             
-            <form class="er_form-x1" action="">
-                <div class="er_wrap-input-x1">
-                    <span class="er_email-x1">E-mail:</span>
-                    <input class="er_input-email-x1" type="email" name="email" id="">
-                    <hr class="hr-x1">
+            <form class="er_form" action="">
+                <div class="er_wrap-input1">
+                    <span class="er_email">E-mail:</span>
+                    <input class="er_input-email" type="email" name="email" id="">
+                    <hr class="hr">
                 </div>
-                <div class="er_wrap-input-x2 ">
-                    <span class="er_fone-x1">Telefone:</span>
-                    <input class="er_input-fone-x1" type="text" name="fone" id="">
-                    <hr class="hr2-x1">
+                <div class="er_wrap-input2 ">
+                    <span class="er_fone">Telefone:</span>
+                    <input class="er_input-fone" type="text" name="fone" id="">
+                    <hr class="hr2">
                 </div>
-                <div class="er_wrap-input-x3">
-                    <span class="er_cpf-x1">Cpf:</span>
-                    <input class="er_input-cpf-x1" type="text" name="cpf" id="">
-                    <hr class="hr3-x1" >
+                <div class="er_wrap-input3">
+                    <span class="er_cpf">Cpf:</span>
+                    <input class="er_input-cpf" type="text" name="cpf" id="">
+                    <hr class="hr3" >
                 </div>
-                <div class="er_wrap-input-x4">
-                    <span class="er_date-x1">Data de Nascimento:</span>
-                    <input class="er_input-date-x1" type="date" name="data" id="">
-                    <hr class="hr4-x1">
+                <div class="er_wrap-input4">
+                    <span class="er_date">Data de Nascimento:</span>
+                    <input class="er_input-date" type="date" name="data" id="">
+                    <hr class="hr4">
                 </div>
                 
 
             </form>
 
-            <button class="er_btn-fixado-x1"><a href="">Fixado</a></button> 
+            <button class="er_btn-fixado"><a href="">Fixado</a></button> 
                 
            
             
