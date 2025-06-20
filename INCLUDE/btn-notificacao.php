a723cb08cb958a7663f20f3ed78036cc50511a33
yuh-maeda <yurimaedanick019@gmail.com>
2025-06-19 20:04:06 -0400

    adicionando funcoes no botao de notificacao

--- INCLUDE/btn-notificacao.php
+++ INCLUDE/btn-notificacao.php
@@ -9,18 +9,36 @@
     <link rel="stylesheet" href="../PUBLIC/css/btn-notificacao.css">
 </head>
 <body>
-    <div class="ym_area-notificacao">
-        <div class="jp_header-icons">
-            <div class="jp_notification-icon">
-                <i class="fas fa-bell"></i>
+
+    <div class="ym_box-notificacao">
+        <div class="ym_area-notificacao">
+            <div class="jp_header-icons">
+                <div class="jp_notification-icon">
+                    <i class="fas fa-bell sino" onclick="teste()"></i>
+                </div>
+                <div class="jp_user-icon">
+                    <i class="fas fa-user"></i>
+                </div>    
             </div>
-            <div class="jp_user-icon">
-                <i class="fas fa-user"></i>
+
+            <div class="ym_notificacao-item">
             </div>
+
         </div>
     </div>
-
-    <div class="area"></div>
-
+    
+        
 </body>
 </html>
+
+<script>
+    
+    function teste(){
+        console.log("teste");
+        let item_not = document.getElementsByClassName('ym_notificacao-item')[0];
+        console.log(item_not)
+        item_not.style.display="flex";
+
+    }
+
+</script>
