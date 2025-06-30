588c24a17be8f20150a30b876fef2b7be423f59d
luacarlos <santoslua226@gmail.com>
2025-06-30 17:01:31 -0400

    correção css

--- VIEW/adm/sobre_prod_adm.php
+++ VIEW/adm/sobre_prod_adm.php
@@ -1,5 +1,5 @@
 <?php
-include "../../INCLUDE/Menu_adm.php";
+include "../../INCLUDE/Menu_vend.php";
 ?>
 
 <!DOCTYPE html>
@@ -15,38 +15,52 @@ include "../../INCLUDE/Menu_adm.php";
 <body>
 
     <main class="jp_main-content">
-        <main class="gs_content">
         <section class="gs_product-container">
-            <div class="gs_product-details">
-                <img src="../../PUBLIC/img/img_produto.png" alt="" class="gs_product-image">
-                <div class="gs_product-info">
+
+            <div class="gs_area-img">
+                <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image">
+                <div class="gs_area-img-select">
+                    <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image-select" id="gs_img-select1">
+                    <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image-select" id="gs_img-select2">
+                    <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image-select" id="gs_img-select3">
+                </div>
+            </div>
+
+            <div class="gs_product-info">
+                <div class="gs_names">
                     <p class="gs_label">Nome</p>
-                    <p class="gs_value"></p>
+                    <p class="gs_value">Forth Equilíbrio</p>
+                </div>
 
+                <div class="gs_names">
                     <p class="gs_label">Categoria</p>
-                    <p class="gs_value">Serviço</p>
+                    <p class="gs_value">Produto</p>
+                </div>
 
+                <div class="gs_names">
                     <p class="gs_label">Preço</p>
                     <p class="gs_value">R$ 99,99</p>
+                </div>
 
+                <div class="gs_names">
                     <p class="gs_label">Estoque</p>
                     <p class="gs_value">50 unidades</p>
-
-                    <p class="gs_label">Descrição</p>
-                    <p class="gs_value"></p>
                 </div>
-            </div>  
+                
+                <div class="gs_names gs_desc">
+                    <p class="gs_label">Descrição</p>
+                    <p class="gs_value gs_desc">Você já está fazendo adubação e mesmo assim a planta não está respondendo legal? Esse adubo pode te ajudar. Ele vai complementar sua adubação principal, levando equilíbrio para o solo e proporcionando melhor absorção dos nutrientes para as suas plantas.</p>
+                </div>
 
-            <a href="../../VIEW/adm/produtos_administrador.php"><button class="gs_back-btn ym_btn-padrao">⟲ Voltar</button></a>
+                <div class="ym_area-btn">
+                    <a href="venda-info-vendedor.php" class="ym_btn-padrao">Comprar</a>
+                    <a href="produtos-tudo.php" class="ym_btn-padrao">Voltar</a>
+                </div>
+            </div>
         </section>
-        </main>
-
-
-
     </main>
 
     <script src="../../PUBLIC/JS/script.js"></script>
-
 
 
     
