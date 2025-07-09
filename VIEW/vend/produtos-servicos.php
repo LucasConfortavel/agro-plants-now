03e0444dd8dbf1a5082df21b5e445eca13633eaa
Sabrina Araujo <sabrinaluana3@gmail.com>
2025-07-08 20:38:34 -0400

    arrumando a cor do produtos-servicos

--- VIEW/vend/produtos-servicos.php
+++ VIEW/vend/produtos-servicos.php
@@ -8,8 +8,9 @@ include "../../INCLUDE/Menu_vend.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos - Serviços</title>
-        <link rel="stylesheet" href="../../PUBLIC/css/style.css">
+    
     <link rel="stylesheet" href="../../PUBLIC/css/style_menu.css">
+    <link rel="stylesheet" href="../../PUBLIC/css/style.css">
     <link rel="stylesheet" href="../../PUBLIC/css/produtos-vendedor.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
