c205b72bde21486ccd02551be2b5b96d180bf877
Sabrina Araujo <sabrinaluana3@gmail.com>
2025-07-08 21:22:35 -0400

    arrumando responsividade

--- VIEW/vend/venda-info-vendedor.php
+++ VIEW/vend/venda-info-vendedor.php
@@ -19,66 +19,68 @@ include "../../INCLUDE/Menu_vend.php";
 <body>
 
     <main class="jp_main-content">
-        <div class="back-button">
-        <a href="lista-vendas.php" class="ym_link-volta"> <i class="fa-solid fa-arrow-left"></i> </a>
-        </div>
-
-        <div class="P_cart-table">
-            <div class="P_table-header">
-                <div class="P_header-produto">Produto</div>
-                <div class="P_header-preco">Preço</div>   
-                <div class="P_header-quantidade">Quantidade</div>
-                <div class="P_header-total">Total</div>
+        <div class="responsividade">
+            <div class="back-button">
+            <a href="lista-vendas.php" class="ym_link-volta"> <i class="fa-solid fa-arrow-left"></i> </a>
             </div>
 
-            <div class="P_cart-item">
-              
-                <div class="P_item-produto">
-                    <div class="P_product-image"></div>
-                    <span>Produto</span>
+            <div class="P_cart-table">
+                <div class="P_table-header">
+                    <div class="P_header-produto">Produto</div>
+                    <div class="P_header-preco">Preço</div>   
+                    <div class="P_header-quantidade">Quantidade</div>
+                    <div class="P_header-total">Total</div>
                 </div>
-                <div class="P_item-preco">R$1000</div>
-                <div class="P_item-quantidade">
-                    <span class="P_quantity-value" id="valor">1</span>
-                </div>
-                <div class="P_item-total">R$1000</div>
-            </div>
-        </div>
 
-        <div class="P_cart-summary">
-
-            <div class="P_details-section">
-                <h3>Detalhes</h3>
+                <div class="P_cart-item">
                 
-                <div class="P_detail-row">
-                    <span>Subtotal</span>
-                    <span class="P_price">R$1000</span>
-                </div>
-                
-                <div class="P_detail-row">
-                    <span>Desconto</span>
-                    <span class="P_price discount">R$0</span>
-                </div>
-                
-                <div class="P_divider"></div>
-                
-                <div class="P_detail-row total-row">
-                    <span>Total</span>
-                    <span class="P_price total">R$1000</span>
+                    <div class="P_item-produto">
+                        <div class="P_product-image"></div>
+                        <span>Produto</span>
+                    </div>
+                    <div class="P_item-preco">R$1000</div>
+                    <div class="P_item-quantidade">
+                        <span class="P_quantity-value" id="valor">1</span>
+                    </div>
+                    <div class="P_item-total">R$1000</div>
                 </div>
             </div>
 
-        </div>
+            <div class="P_cart-summary">
 
-        <div class="P_customer-info">
-            <div class="P_customer-details">
-                <div class="P_customer-label">Cliente</div>
-                <div class="P_customer-name">
-                    Rafael Germinari
+                <div class="P_details-section">
+                    <h3>Detalhes</h3>
+                    
+                    <div class="P_detail-row">
+                        <span>Subtotal</span>
+                        <span class="P_price">R$1000</span>
+                    </div>
+                    
+                    <div class="P_detail-row">
+                        <span>Desconto</span>
+                        <span class="P_price discount">R$0</span>
+                    </div>
+                    
+                    <div class="P_divider"></div>
+                    
+                    <div class="P_detail-row total-row">
+                        <span>Total</span>
+                        <span class="P_price total">R$1000</span>
+                    </div>
                 </div>
-                <div class="P_customer-date">
-                    <div>Data de criação</div>
-                    <div>16/08</div>
+
+            </div>
+
+            <div class="P_customer-info">
+                <div class="P_customer-details">
+                    <div class="P_customer-label">Cliente</div>
+                    <div class="P_customer-name">
+                        Rafael Germinari
+                    </div>
+                    <div class="P_customer-date">
+                        <div>Data de criação</div>
+                        <div>16/08</div>
+                    </div>
                 </div>
             </div>
         </div>
