fe13bc279bf4fbe11c342de694ee09feda6acead
yuh-maeda <yurimaedanick019@gmail.com>
2025-07-30 16:34:56 -0400

    corrigindo pop-up

--- VIEW/pop-up/pop-up_cupom_Adicionado.php
+++ VIEW/pop-up/pop-up_cupom_Adicionado.php
@@ -8,22 +8,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
-<body>
-    <section class="ym_area-pop-up">
-        <div class="ym_pop-up">
-            <div class="ym_superior-pop-up">
-                <h1 class='ym_titulo'>Cupom aplicado <i class="fa-solid fa-ticket"></i></h1>
-                <button class="ym_btn-pop-up" style="margin-bottom: 20px;"><img clas="ym_img-icons" src="../../PUBLIC/img/fecha.png" alt=""></button>
-            </div>
-            
-            <div class="ym_inferior-pop-up">
-                <p id="ym_link">Um desconto de 10% foi adicionado a compra</p>
-
-            </div>
-            
-        </div>
-    </section>
-    
+<body>        
+        <div class="ym_inferior-pop-up">
+            <p id="ym_link">Um desconto de 10% foi adicionado a compra</p>
+        </div>        
 </body>
 
 </html>
