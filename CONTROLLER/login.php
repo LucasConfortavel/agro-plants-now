85aec0e5927e0552b0c51227a0107f09593b1912
Luf3_ <luf3developer@proton.me>
2025-09-10 14:57:47 -0400

    correção login logout

--- CONTROLLER/login.php
+++ CONTROLLER/login.php
@@ -2,5 +2,13 @@
 require_once 'UsuarioController.php';
 
 $controller = new UsuarioController();
-$controller->login();
+$result = $controller->login();
+
+if (isset($result['redirect'])) {
+    header('Location: ' . $result['redirect']);
+    exit;
+} else if (isset($result['error'])) {
+    header('Location: ../../VIEW/paginas-iniciais/pagina-de-login.php?error=' . urlencode($result['error']));
+    exit;
+}
 ?>
