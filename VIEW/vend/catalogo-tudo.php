e04d93fd5e4393cb4279f7b562b2b9bb1249b355
Luf3_ <luf3developer@proton.me>
2025-09-12 15:23:38 -0400

    catalogo vendedor dinamico

--- VIEW/vend/catalogo-tudo.php
+++ VIEW/vend/catalogo-tudo.php
@@ -1,5 +1,19 @@
 <?php
 include "../../INCLUDE/Menu_vend.php";
+
+require_once '../../CONTROLLER/ProdutoController.php';
+require_once '../../CONTROLLER/ServicoController.php';
+
+$produtoController = new ProdutoController();
+$servicoController = new ServicoController();
+
+$produtos = $produtoController->index();
+$servicos = $servicoController->index();
+
+$erroProdutos = isset($produtos['error']);
+$erroServicos = isset($servicos['error']);
+
+$imagemPadrao = '../../PUBLIC/img/img_produto.png';
 ?>
 
 <!DOCTYPE html>
@@ -16,106 +30,111 @@ include "../../INCLUDE/Menu_vend.php";
 <body>
 
     <main class="jp_main-content">
+        <section class="ym_sectionProdutos">
+            <h1 class="ym_titulo">Catálogo - Geral</h1>
 
-        <?php $nome_produto = 'Nome do produto'; $imagem_produto = '../../PUBLIC/img/img_produto.png';?>
-        <?php $nome_produto = 'Nome do produto'; $imagem_produto2 = '../../PUBLIC/img/img_o-que-fazemos.png';?>
-        
-            <section class="ym_sectionProdutos">
-    
-                <h1 class="ym_titulo">Catálogo - Geral</h1>
-
-                <div class="ym_categorias">
+            <div class="ym_categorias">
+                <div class="ym_area-input-pesquisa">
+                    <a href="" class="ym_lupa"><i class="fa-solid fa-magnifying-glass"></i></a>
+                    <input type="text" placeholder="Pesquise por algo no catálogo" class="ym_produtoPesquisa" id="inputPesquisa">    
+                </div>  
+                
+                <div class="ym_area-select">
+                    <div class="ym_select" onclick="mostrar_categorias()">
+                        <p class="ym_categoria-select">Todos</p>
+                        <p class="ym_seta-categoria">></p>
+                    </div>
                     
-                    <div class="ym_area-input-pesquisa">
-                        <a href="" class="ym_lupa"><i class="fa-solid fa-magnifying-glass"></i></a>
-                        <input  type="text" placeholder="Pesquise por algo no catálogo" class="ym_produtoPesquisa">    
-                    </div>  
-                    
-                    
-                    <div class="ym_area-select">
-                        <div class="ym_select" onclick="mostrar_categorias()">
-                            <p class="ym_categoria-select">Todos</p>
-                            <p class="ym_seta-categoria">></p>
-                        </div>
-                        
-                        
-                        <div class="ym_options">
-                            <a href="catalogo-produtos.php" class="ym_link-option"><i class="fa-solid fa-building-wheat"></i> produto</a>
-                            <a href="catalogo-servicos.php" class="ym_link-option"><i class="fa-solid fa-users-gear"></i> serviço</a>
-                        </div>
+                    <div class="ym_options">
+                        <a href="catalogo-produtos.php" class="ym_link-option"><i class="fa-solid fa-building-wheat"></i> produto</a>
+                        <a href="catalogo-servicos.php" class="ym_link-option"><i class="fa-solid fa-users-gear"></i> serviço</a>
                     </div>
                 </div>
+            </div>
+            
+            <p class="ym_textoArea">Principais produtos</p>
                 
-                <p class="ym_textoArea">Principais produtos</p>
-                    
-                    <div class="ym_areaProdutos">
-                        <div class="ym_todos-produtos">
-                            <?php for($i=0; $i < 20; $i++){echo'
-                                <div class="ym_cardProduto">
-                                    <div class="ym_img-placeholder">
-                                        <img src="'. $imagem_produto .'" alt="umg-produto" class="ym_img">
-                                        <div class="ym_img-label">
-                                            <span>Bioestimulante</span>
-                                        </div>
-                                    </div>
-                                    <p class="ym_nomeProduto">'. $nome_produto .'</p>
-                                    <p class="ym_preco">R$ 120,00</p>
-                                    <p class="ym_descricao">Estimula o crescimento e a produtividade das plantas.</p>
-                                    <a href="sobre_prod.php?id='.$i.'" class="ym_linkProduto ym_btn-padrao">Veja mais</a>
-                                </div>';}
-                                ?>
-                        </div>
-                        
-                        <?php echo'
-                        <div class="ym_btn-slide-area">
-                            <button class="ym_btn-slide ym_slideBack" onclick="slideBack('.$i.',0)"> < </button>
-                            <button class="ym_btn-slide ym_slideGo" onclick="slideGo('.$i.',0)"> > </button>
-                        </div>'
-                        ?>
-                        
+            <div class="ym_areaProdutos">
+                <?php if ($erroProdutos): ?>
+                    <div class="ym_erro">
+                        Erro ao carregar produtos: <?php echo $produtos['error']; ?>
                     </div>
+                <?php else: ?>
+                    <div class="ym_todos-produtos" id="produtos-container">
+                        <?php 
+                        $contadorProdutos = 0;
+                        foreach ($produtos as $produto): 
+                            $contadorProdutos++;
+                        ?>
+                            <div class="ym_cardProduto">
+                                <div class="ym_img-placeholder">
+                                    <img src="<?php echo $imagemPadrao; ?>" alt="img-produto" class="ym_img">
+                                    <div class="ym_img-label">
+                                        <span><?php echo htmlspecialchars($produto['nome']); ?></span>
+                                    </div>
+                                </div>
+                                <p class="ym_nomeProduto"><?php echo htmlspecialchars($produto['nome']); ?></p>
+                                <p class="ym_preco">R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></p>
+                                <p class="ym_descricao"><?php echo htmlspecialchars($produto['descricao'] ?? 'Sem descrição'); ?></p>
+                                <a href="sobre_prod.php?id=<?php echo $produto['id']; ?>" class="ym_linkProduto ym_btn-padrao">Veja mais</a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    
+                    <?php if ($contadorProdutos > 0): ?>
+                    <div class="ym_btn-slide-area">
+                        <button class="ym_btn-slide ym_slideBack" onclick="slideBack(<?php echo $contadorProdutos; ?>, 0)"> < </button>
+                        <button class="ym_btn-slide ym_slideGo" onclick="slideGo(<?php echo $contadorProdutos; ?>, 0)"> > </button>
+                    </div>
+                    <?php else: ?>
+                    <p class="ym_sem-itens">Nenhum produto encontrado.</p>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </div>
+
+            <p class="ym_textoArea">Principais serviços</p>
                 
-
-
-
-
-                <p class="ym_textoArea">Principais   serviços</p>
-                    
-
-                <div class="ym_areaProdutos">
-                        <div class="ym_todos-produtos">
-                            <?php for($i=0; $i < 20; $i++){echo'
-                                <div class="ym_cardProduto">
-                                    <div class="ym_img-placeholder">
-                                        <img src="'. $imagem_produto .'" alt="umg-produto" class="ym_img">
-                                        <div class="ym_img-label">
-                                            <span>Bioestimulante</span>
-                                        </div>
-                                    </div>
-                                    <p class="ym_nomeProduto">'. $nome_produto .'</p>
-                                    <p class="ym_preco">R$ 120,00</p>
-                                    <p class="ym_descricao">Estimula o crescimento e a produtividade das plantas.</p>
-                                    <a href="#" class="ym_linkProduto ym_btn-padrao">Veja mais</a>
-                                </div>';}
-                                ?>
-                        </div>
-                        
-                        <?php echo'
-                        <div class="ym_btn-slide-area">
-                            <button class="ym_btn-slide ym_slideBack" onclick="slideBack('.$i.',1)"> < </button>
-                            <button class="ym_btn-slide ym_slideGo" onclick="slideGo('.$i.',1)"> > </button>
-                        </div>'
-                        ?>
-                        
+            <div class="ym_areaProdutos">
+                <?php if ($erroServicos): ?>
+                    <div class="ym_erro">
+                        Erro ao carregar serviços: <?php echo $servicos['error']; ?>
                     </div>
-
-            </section>
+                <?php else: ?>
+                    <div class="ym_todos-produtos" id="servicos-container">
+                        <?php 
+                        $contadorServicos = 0;
+                        foreach ($servicos as $servico): 
+                            $contadorServicos++;
+                        ?>
+                            <div class="ym_cardProduto">
+                                <div class="ym_img-placeholder">
+                                    <img src="<?php echo $imagemPadrao; ?>" alt="img-servico" class="ym_img">
+                                    <div class="ym_img-label">
+                                        <span><?php echo htmlspecialchars($servico['nome']); ?></span>
+                                    </div>
+                                </div>
+                                <p class="ym_nomeProduto"><?php echo htmlspecialchars($servico['nome']); ?></p>
+                                <p class="ym_preco">R$ <?php echo number_format($servico['preco'], 2, ',', '.'); ?></p>
+                                <p class="ym_descricao"><?php echo htmlspecialchars($servico['descricao'] ?? 'Sem descrição'); ?></p>
+                                <a href="sobre_serv.php?id=<?php echo $servico['id']; ?>" class="ym_linkProduto ym_btn-padrao">Veja mais</a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    
+                    <?php if ($contadorServicos > 0): ?>
+                    <div class="ym_btn-slide-area">
+                        <button class="ym_btn-slide ym_slideBack" onclick="slideBack(<?php echo $contadorServicos; ?>, 1)"> < </button>
+                        <button class="ym_btn-slide ym_slideGo" onclick="slideGo(<?php echo $contadorServicos; ?>, 1)"> > </button>
+                    </div>
+                    <?php else: ?>
+                    <p class="ym_sem-itens">Nenhum serviço encontrado.</p>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </div>
+        </section>
     </main>
-
-
 </body>
 </html>
 
 <script src="../../PUBLIC/JS/script.js"></script>
-
 <script src="../../PUBLIC/JS/script-catalogo.js"></script>
--- VIEW/vend/sobre_prod.php
+++ VIEW/vend/sobre_prod.php
@@ -1,17 +1,32 @@
 <?php
-    include "../../INCLUDE/Menu_vend.php";
-?>
+include "../../INCLUDE/Menu_vend.php";
 
-</body>
-    <script src="../../PUBLIC/JS/script-menus-laterais.js"></script>
-</html>
+require_once '../../CONTROLLER/ProdutoController.php';
+
+if (!isset($_GET['id']) || empty($_GET['id'])) {
+    header('Location: catalogo-tudo.php');
+    exit;
+}
+
+$id_produto = $_GET['id'];
+
+$produtoController = new ProdutoController();
+$produto = $produtoController->mostrar($id_produto);
+
+if (isset($produto['error'])) {
+    header('Location: catalogo-tudo.php?error=' . urlencode($produto['error']));
+    exit;
+}
+
+$imagemPadrao = '../../PUBLIC/img/img_produto.webp';
+?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informações do Produto</title>
+    <title>Informações do Produto - <?php echo htmlspecialchars($produto['nome']); ?></title>
     <link rel="stylesheet" href="../../PUBLIC/css/sobre_prod.css">
     <link rel="stylesheet" href="../../PUBLIC/css/style_menu.css">
     <link rel="stylesheet" href="../../PUBLIC/css/style.css">
@@ -23,18 +38,18 @@
             <section class="gs_product-container">
                 
                 <div class="gs_area-img">
-                    <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image">
+                    <img src="<?php echo $imagemPadrao; ?>" alt="<?php echo htmlspecialchars($produto['nome']); ?>" class="gs_product-image">
                     <div class="gs_area-img-select">
-                        <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image-select" id="gs_img-select1">
-                        <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image-select" id="gs_img-select2">
-                        <img src="../../PUBLIC/img/img_produto.webp" alt="" class="gs_product-image-select" id="gs_img-select3">
+                        <img src="<?php echo $imagemPadrao; ?>" alt="Imagem 1" class="gs_product-image-select" id="gs_img-select1">
+                        <img src="<?php echo $imagemPadrao; ?>" alt="Imagem 2" class="gs_product-image-select" id="gs_img-select2">
+                        <img src="<?php echo $imagemPadrao; ?>" alt="Imagem 3" class="gs_product-image-select" id="gs_img-select3">
                     </div>
                 </div>
 
                 <div class="gs_product-info">
                     <div class="gs_names">
                         <p class="gs_label">Nome</p>
-                        <p class="gs_value">Forth Equilíbrio</p>
+                        <p class="gs_value"><?php echo htmlspecialchars($produto['nome']); ?></p>
                     </div>
 
                     <div class="gs_names">
@@ -44,21 +59,26 @@
 
                     <div class="gs_names">
                         <p class="gs_label">Preço</p>
-                        <p class="gs_value">R$ 99,99</p>
+                        <p class="gs_value">R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></p>
                     </div>
 
                     <div class="gs_names">
                         <p class="gs_label">Estoque</p>
-                        <p class="gs_value">50 unidades</p>
+                        <p class="gs_value"><?php echo htmlspecialchars($produto['quantidade']); ?> unidades</p>
+                    </div>
+                    
+                    <div class="gs_names">
+                        <p class="gs_label">Reservado</p>
+                        <p class="gs_value"><?php echo htmlspecialchars($produto['reservado']); ?> unidades</p>
                     </div>
                     
                     <div class="gs_names gs_desc">
                         <p class="gs_label">Descrição</p>
-                        <p class="gs_value gs_desc">Você já está fazendo adubação e mesmo assim a planta não está respondendo legal? Esse adubo pode te ajudar. Ele vai complementar sua adubação principal, levando equilíbrio para o solo e proporcionando melhor absorção dos nutrientes para as suas plantas.</p>
+                        <p class="gs_value gs_desc"><?php echo htmlspecialchars($produto['descricao'] ?? 'Sem descrição disponível.'); ?></p>
                     </div>
 
                     <div class="ym_area-btn">
-                        <a href="venda-info-vend.php" class="ym_btn-padrao">Comprar</a>
+                        <a href="venda-info-vend.php?produto_id=<?php echo $produto['id']; ?>" class="ym_btn-padrao">Comprar</a>
                         <a href="catalogo-tudo.php" class="ym_btn-padrao">Voltar</a>
                     </div>
                 </div>
@@ -67,9 +87,6 @@
     </main>
 
     <script src="../../PUBLIC/JS/script.js"></script>
-
-
-    
-
+    <script src="../../PUBLIC/JS/script-sobre-prod.js"></script>
 </body>
 </html>
