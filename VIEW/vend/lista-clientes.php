24faa8e9587fc2cc15e48dbe277adc2e84a340f8
Eduardo31-del <eduardoribeirolopes472@gmail.com>
2025-11-06 16:12:47 -0400

    update-btn-padrao
    padronizaçao das telas iniciais e vend

--- VIEW/vend/lista-clientes.php
+++ VIEW/vend/lista-clientes.php
@@ -117,7 +117,6 @@ if(isset($_SESSION['alerta'])){
     <link rel="stylesheet" href="../../PUBLIC/css/clientes-adm.css">
     <link rel="stylesheet" href="../../PUBLIC/css/style_menu.css">
     <link rel="stylesheet" href="../../PUBLIC/css/style.css">
-    <link rel="stylesheet" href="../../PUBLIC/css/pagina-de-login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
