3333d43bfa840d461297e70e540dbb2c3abc33fa
YunoKamai <sabrinaluana3@gmail.com>
2025-11-25 15:25:38 -0400

    rebase

--- INCLUDE/alertas.php
+++
@@ -1,8 +0,0 @@
-<div class="ym_area-alertas"></div>
-<script src="../../PUBLIC/JS/script-alertas.js"></script>
-<?php
-    if(isset($_SESSION['alerta'])){
-        echo($_SESSION['alerta']);
-        unset($_SESSION['alerta']);
-    }
-?>
--- PUBLIC/JS/script_btn-notificacao.js
+++
@@ -1,5 +0,0 @@
-const notificacao = document.getElementsByClassName('ym_area-notificacao')[0];
-notificacao.addEventListener('click', () => {
-    notificacao.classList.toggle('active');
-    $('.jp_notification-icon').find('i').toggleClass('fa-xmark');
-});
